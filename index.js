0b08491cee2b98f33c508695232efedfdcaccde7
Nikita Vasilyev <me@elv1s.ru>
2012-10-01 14:14:39 +0400

    update my position every second. doesnt work yet

--- index.js
+++ index.js
@@ -28,25 +28,42 @@ function updateMarkerAddress(str) {
 }
 
 function initialize() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var latLng = new google.maps.LatLng(latitude, longitude);
-    var map = new google.maps.Map(document.getElementById('mapCanvas'), {
-      zoom: 12,
-      center: latLng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-    var marker = new google.maps.Marker({
-      position: latLng,
-      title: 'Point A',
-      map: map
-    });
-
-    // Update current position info.
-    updateMarkerPosition(latLng);
-    geocodePosition(latLng);
+  navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError, {
+    enableHighAccuracy: true
   });
+}
+
+function onGeolocationSuccess(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  var latLng = new google.maps.LatLng(latitude, longitude);
+  var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+    zoom: 14,
+    center: latLng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  var marker = new google.maps.Marker({
+    position: latLng,
+    title: 'Point A',
+    map: map
+  });
+
+  // Update current position info.
+  updateMarkerPosition(latLng);
+  geocodePosition(latLng);
+
+  setInterval(function update() {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      if (pos.coords.latitude != latitude || pos.coords.longitude != longitude) {
+        //marker.setPosition({latitude: latitude, longitude: longitude});
+        // What's the format of the arguments
+      }
+    });
+  }, 1000);
+}
+
+function onGeolocationError(error) {
+  console.error(error.message, error.code);
 }
 
 // Onload handler to fire off the app.
